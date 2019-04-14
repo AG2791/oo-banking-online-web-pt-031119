829b48b6a977a26ff7e62f67a55ad5ac85efb69f
AJ Guiles <ashiguiles@gmail.com>
2019-04-14 21:56:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,6 +22,18 @@ def execute transaction
       reject_transfer
     end
  end
+ 
+ def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+  
+  
   
   
 end
